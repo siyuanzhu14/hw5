6502b309b465299e584e5dcb89cb706d651a363f
STEP Student <carinazhu94@gmail.com>
2019-07-05 10:02:17 +0900

    BFS

--- go/app.go
+++ go/app.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"html/template"
 	"net/http"
-
+    "fmt"
+    
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
 )
 
 func init() {
-	http.HandleFunc("/", handleExample)
+	http.HandleFunc("/", handlePata)
 	http.HandleFunc("/norikae", handleNorikae)
 }
 
@@ -25,7 +26,8 @@ type Page struct {
 	Pata string
 }
 
-func handleExample(w http.ResponseWriter, r *http.Request) {
+
+func handlePata(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	// templateに埋める内容をrequestのFormValueから用意する。
@@ -36,8 +38,26 @@ func handleExample(w http.ResponseWriter, r *http.Request) {
 
 	// とりあえずPataを簡単な操作で設定しますけど、すこし工夫をすれば
 	// パタトクカシーーができます。
-	content.Pata = content.A + content.B
-
+	//content.Pata = content.A + content.B
+	x := ""
+    Arune := []rune(content.A)
+    Brune := []rune(content.B)
+    if len(Arune) <= len(Brune){
+        for i:=0; i<len(Arune); i++ {
+            x = x + string(Arune[i]) + string(Brune[i])
+        }
+        for i:=len(Arune); i<len(Brune); i++{
+            x = x + string(Brune[i])
+        }
+    }else { for i:=0; i<len(Brune); i++  {
+            x = x + string(Arune[i]) + string(Brune[i])
+        }  
+        for i:=len(Brune); i<len(Arune); i++{
+            x = x + string(Arune[i])
+        }
+    }     
+    content.Pata = x
+    fmt.Printf(x)
 	// example.htmlというtemplateをcontentの内容を使って、{{.A}}などのとこ
 	// ろを実行して、内容を埋めて、wに書き込む。
 	tmpl.ExecuteTemplate(w, "example.html", content)
@@ -50,9 +70,132 @@ type Line struct {
 	Stations []string
 }
 
+type Route struct {
+    Start  string
+    End  string
+    Network TransitNetwork
+    Connected map[string][]Eki
+    
+    //Result TransitNetwork
+    Result []Eki
+    //Fastest []TransitNetwork
+}
+
 // TransitNetworkは http://fantasy-transit.appspot.com/net?format=json
 // の一番外側のリストのことを表しています。
 type TransitNetwork []Line
+
+
+
+type Eki struct {
+    LineName    string
+    EkiName   string
+}
+
+func BuildEkiUnits(network TransitNetwork) [][]Eki{
+    
+    var AllStations [][]Eki
+    
+    for _, line:= range network{
+        
+        ekis := line.Stations
+       
+        var temp []Eki
+        for j:=0; j< len(ekis); j++ {
+            var eki Eki
+            eki.EkiName = ekis[j]
+            eki.LineName = line.Name
+            temp = append(temp, eki)
+            
+        }
+        AllStations = append(AllStations, temp)
+    }
+    
+    return AllStations
+}
+
+func BuildConnected(ekiUnits [][]Eki)(map[string][]Eki){
+    
+    connected := make(map[string][]Eki)
+    for _, line:= range ekiUnits{
+       
+        for i:=0; i< len(line); i++ {
+            
+            eki := line[i].EkiName
+            
+               
+                if i > 0 && i <= len(line) - 2{
+                    connected[eki] = append(connected[eki], line[i-1], line[i+1])
+                }else if i == 0{
+                    
+                    connected[eki] = append(connected[eki], line[i+1])
+                    
+                }else{
+                    connected[eki] = append(connected[eki], line[i-1])
+                }
+                
+                
+            }
+        }
+    return connected
+}
+
+func makeEki(name string)(Eki){
+    res := Eki{
+        EkiName: name,
+        LineName: "whatever",
+    }
+    return res
+}
+
+func EkiInRoute(a string, list []Eki) bool {
+    for _, b := range list {
+        if b.EkiName == a {
+            return true
+        }
+    }
+    return false
+}
+
+
+//a:start station. b:end station
+func BFS(connected map[string][]Eki, start string, end string) []Eki{
+    
+    q := make([][]Eki,0)
+    q = append(q, []Eki{makeEki(start)})
+    route := make([]Eki,0)
+    
+    for len(q) != 0 {
+        
+        route = q[0]
+        here := route[len(route)-1].EkiName  
+
+        if here == end {
+            return route
+        }
+          
+        //a := len(q)
+        
+        
+        newq := make([][]Eki,0)
+        newroute := make([]Eki,len(route)+1)
+        for _, temp := range connected[here]{
+          go func(newEki Eki){
+                if EkiInRoute(newEki.EkiName, route) == false{
+                    newroute = append(route, newEki)
+                    newq = append(newq, newroute)
+                }      
+            }(temp)
+        }
+        q = append(q, newq...)
+        
+        q = q[1:]   
+    
+    
+    }
+    return nil
+    
+}
 
 func handleNorikae(w http.ResponseWriter, r *http.Request) {
 	// Appengineの「Context」を通してAppengineのAPIを利用する。
@@ -79,5 +222,24 @@ func handleNorikae(w http.ResponseWriter, r *http.Request) {
 
 	// handleExampleと同じようにtemplateにテンプレートを埋めて、出力する。
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tmpl.ExecuteTemplate(w, "norikae.html", network)
+    
+
+    content := Route{
+		Start: r.FormValue("a"),
+		End: r.FormValue("b"),
+	}
+    
+
+    content.Network = network
+
+    ekiUnits := BuildEkiUnits(content.Network)
+    connected := BuildConnected(ekiUnits)
+    
+
+    
+    content.Connected = connected
+    result := BFS(content.Connected, content.Start, content.End) 
+    content.Result  = result 
+	
+    tmpl.ExecuteTemplate(w, "norikae.html", content)
 }
---
+++ go/app_test.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+
+func TestPata(t *testing.T) {
+	tests := []struct {
+		a, b string
+		want string
+	}{
+		{"パトカー", "タクシー", "パタトクカシーー"},
+		{"hamster", "lobster", "hlaombsstteerr"},
+        {"dielvls", "パトカー", "dパiトeカlーvls"},
+        {"パトカー", "dielvls", "パdトiカeーlvls"},
+	}
+	for _, test := range tests {
+		req, _ := http.NewRequest("GET", "/pata", nil)
+		req.Form = make(url.Values)
+		req.Form["a"] = []string{test.a}
+		req.Form["b"] = []string{test.b}
+		w := httptest.NewRecorder()
+		handlePata(w, req)
+        //fmt.Print(w.Body.String())
+		if !strings.Contains(w.Body.String(), test.want) {
+			t.Errorf("/pata with a=%v b=%v got: %v wanted %v", test.a, test.b, w.Body.String(), test.want)
+		} 
+	}
+}
+
